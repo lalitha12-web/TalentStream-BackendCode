e40341527ac58160a25fde4e0a8841899e65930e
lalitha12-web <65554162+lalitha12-web@users.noreply.github.com>
2023-09-05 18:01:48 +0530

    Delete main directory

--- main/java/com/bitlabs/service/ProfileService.java
+++
@@ -1,8 +0,0 @@
-package com.bitlabs.service;
-
-import com.bitlabs.entity.Profile;
-
-public interface ProfileService {
-	 public Profile saveProfile(Profile profile);
-
-}
--- main/java/com/bitlabs/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.bitlabs.service;
-
-import com.bitlabs.entity.User;
-
-public interface UserService {
-	boolean adminLogin(User user);
-	boolean login(User user);
-     User addUser(User user);
-}
