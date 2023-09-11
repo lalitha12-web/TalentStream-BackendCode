9f7a1efdc58d255644fb90d98b3ba73909870869
lalitha12-web <65554162+lalitha12-web@users.noreply.github.com>
2023-09-11 11:44:57 +0530

    Delete TalentStream directory

--- TalentStream/src/main/java/com/bitlabs/repo/ProfileRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bitlabs.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.bitlabs.entity.Profile;
-
-public interface ProfileRepository extends JpaRepository<Profile, Long> {
-
-}
